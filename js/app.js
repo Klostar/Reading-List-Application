06024fe4422fb6e1e7c28da35e1cfb340193cfb7
chloe alcorn <ca.alcorn@outlook.com>
2018-09-06 15:12:05 +0100

    can add multiple entries

--- js/app.js
+++ js/app.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // console.log('JavaScript loaded');
   const form = document.querySelector('#new-item-form');
   form.addEventListener('submit', handleSubmit);
-  form.reset();
 });
 
 const handleSubmit = function () {
   event.preventDefault();
   const resultMessage = `The title is: ${this.title.value} The Author is: ${this.author.value} The category is : ${this.category.value}`
 
-  const formResult = document.querySelector('#reading-list');
-  formResult.textContent = resultMessage;
+  const newListItem = document.createElement('li');
+  newListItem.textContent = resultMessage;
+
+
+  const formResult = document.querySelector('ul');
+  formResult.appendChild(newListItem);
+
 
 }
